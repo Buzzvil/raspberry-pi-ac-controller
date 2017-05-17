0798fc125d7f6926f26e3281a02bdd5a521979bb
SeoJungHong <hsj0223@gmail.com>
2017-05-17 22:49:41 +0900

    add api for light/

--- ac/api/urls.py
+++ ac/api/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^hello/', views.hello),
     url(r'^ac/on/', views.ac_on),
     url(r'^ac/off/', views.ac_off),
+    url(r'^light/on/', views.light_on),
+    url(r'^light/off/', views.light_on),
+    url(r'^light/color/(?P<color>[A-Z])', views.light_color),
 ]
--- ac/api/views.py
+++ ac/api/views.py
@@ -19,3 +19,14 @@ def ac_on(request):
 def ac_off(request):
     call(["irsend", "SEND_ONCE", "ac_lg", "BTN_0"])
     return JsonResponse({})
+
+def light_on(request):
+    call(["irsend", "SEND_ONCE", "molang", "KEY_ON"])
+    return JsonResponse({})
+
+def light_color(request, color):
+    if not color in ('R', 'G', 'B'):
+        return JsonResponse({'error': 'wrong color'})
+    keyname = "KEY_" + color
+    call(["irsend", "SEND_ONCE", "molang", keyname])
+    return JsonResponse({})
