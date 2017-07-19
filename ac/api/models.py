2d777151be95f7c021af6366fdb622f38bc15fa6
zuneseo <zuneseo@buzzvil.com>
2017-07-19 22:44:57 +0900

    불이 켜진 상태에서 꺼진 경우만 acoff api 호출

--- ac/api/models.py
+++ ac/api/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class LightStatus(models.Model):
+    is_light_on = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- ac/api/tasks.py
+++ ac/api/tasks.py
@@ -5,6 +5,7 @@ import os
 import requests
 from celery import shared_task
 
+from api.models import LightStatus
 from api.utils import LightSensor
 
 logger = logging.getLogger(__name__)
@@ -25,11 +26,20 @@ def check_light():
         if light_level < THRESHOLD_LIGHT_LEVEL:
             count += 1
 
+    light_status = LightStatus.objects.get(pk=1)
     if count > MIN_COUNT:
         logger.info('Light is off - Turn off the AC (count : {0}/{1})'.format(count, WINDOW_RANGE))
-        requests.post('https://za4ekgzk6b.execute-api.us-east-1.amazonaws.com/Prod/slack', {
-            'token': os.environ['SLACK_TOKEN'],
-            'command': '/acoff'
-        })
+
+        if light_status.is_light_on:
+            requests.post('https://za4ekgzk6b.execute-api.us-east-1.amazonaws.com/Prod/slack', {
+                'token': os.environ['SLACK_TOKEN'],
+                'command': '/acoff',
+            }, timeout=30)
+            light_status.is_light_on = False
+            light_status.save()
     else:
         logger.info('Light is on')
+        if not light_status.is_light_on:
+            logger.info('update light status on')
+            light_status.is_light_on = True
+            light_status.save()
