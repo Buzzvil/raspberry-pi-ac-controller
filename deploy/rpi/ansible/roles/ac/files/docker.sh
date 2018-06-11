253dcc46c6815f3c3bc4f0611f6ed85d2b3c1b4d
Jueun Seo <jeseo1@gmail.com>
2018-06-11 21:00:53 +0900

    lirc 0.9.4c 대응

--- deploy/rpi/ansible/roles/ac/files/docker.sh
+++ deploy/rpi/ansible/roles/ac/files/docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -ex
 
-docker pull buzzvil/ac
+docker pull buzzvil/ac:2018-06-07
 docker run \
-	-d \
-	-v /master_env.sh:/master_env.sh:ro \
+    -d \
+    -v /master_env.sh:/master_env.sh:ro \
     -v /var/run/lirc/lircd:/var/run/lirc/lircd:ro \
     --device=/dev/i2c-1 \
-	-p 8833:8833 \
-	--restart always \
-	--name ac \
-    buzzvil/ac
+    -p 8833:8833 \
+    --restart always \
+    --log-opt max-size=10m \
+    --log-opt max-file=10 \
+    --name ac \
+    buzzvil/ac:2018-06-07
